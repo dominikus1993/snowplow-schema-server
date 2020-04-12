90402b38e44004792db4bf3314ac845350e26cdd
Dominik.Kotecki <dominikus1910@gmail.com>
2020-04-12 20:53:29 +0000

    add datadog

--- main.go
+++ main.go
@@ -1,19 +1,45 @@
 package main
 
 import (
-	"flag"
-	"log"
+	"net"
 	"net/http"
-	"strings"
+	"os"
+
+	log "github.com/sirupsen/logrus"
+	httptrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/net/http"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 )
 
+// GetEnvOrDefault gives env variable name or default value
+func GetEnvOrDefault(key, defaultValue string) string {
+	val := os.Getenv(key)
+	if val != "" {
+		return val
+	}
+	return defaultValue
+}
+
+var PATH_BASE string = GetEnvOrDefault("PATH_BASE", "/")
+var PORT string = GetEnvOrDefault("PORT", "3000")
+var ENV string = GetEnvOrDefault("ENV", "dev")
+
 func main() {
-	port := flag.String("p", "3000", "port to serve on")
-	directory := flag.String("d", ".", "the directory of static file to host")
-	flag.Parse()
 
-	http.Handle("/statics/", http.StripPrefix(strings.TrimRight("/statics/", "/"), http.FileServer(http.Dir(*directory))))
+	log.SetFormatter(&log.JSONFormatter{})
+	addr := net.JoinHostPort(
+		os.Getenv("DD_AGENT_HOST"),
+		os.Getenv("DD_TRACE_AGENT_PORT"),
+	)
+	// start the tracer with zero or more options
+	tracer.Start(tracer.WithServiceName("snowplow-schema-server"), tracer.WithAgentAddr(addr), tracer.WithEnv(ENV))
+	defer tracer.Stop()
+	mux := httptrace.NewServeMux() // init the http tracer
 
-	log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
-	log.Fatal(http.ListenAndServe(":"+*port, nil))
+	mux.Handle(PATH_BASE, http.FileServer(http.Dir("./schemas")))
+
+	log.Println("Start listening on port:", PORT)
+	err := http.ListenAndServe(":"+PORT, mux) // set listen port
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
