220cc711a01f37baab669572b2fe430fdc27d87d
Dominik.Kotecki <dominikus1910@gmail.com>
2020-04-13 14:05:34 +0000

    use chi instead of mux

--- go.mod
+++ go.mod
@@ -4,10 +4,11 @@ go 1.14
 
 require (
 	github.com/DataDog/datadog-go v3.5.0+incompatible // indirect
+	github.com/go-chi/chi v4.1.0+incompatible
 	github.com/philhofer/fwd v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.5.0
 	github.com/tinylib/msgp v1.1.2 // indirect
 	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
 	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
-	gopkg.in/DataDog/dd-trace-go.v1 v1.23.0
+	gopkg.in/DataDog/dd-trace-go.v1 v1.23.1
 )
--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"log"
 	"net"
 	"net/http"
 	"os"
 
-	log "github.com/sirupsen/logrus"
-	httptrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/net/http"
+	mylogger "snowplow-schema-server/app/logger"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/sirupsen/logrus"
+	chitrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/go-chi/chi"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 )
 
@@ -23,9 +28,16 @@ var PATH_BASE string = GetEnvOrDefault("PATH_BASE", "/")
 var PORT string = GetEnvOrDefault("PORT", "3000")
 var ENV string = GetEnvOrDefault("ENV", "dev")
 
-func main() {
+func NewStructuredLogger(logger *logrus.Logger) func(next http.Handler) http.Handler {
+	return middleware.RequestLogger(&mylogger.StructuredLogger{Logger: logger})
+}
 
-	log.SetFormatter(&log.JSONFormatter{})
+func main() {
+	logger := logrus.New()
+	logger.Formatter = &logrus.JSONFormatter{
+		// disable, as we set our own
+		DisableTimestamp: true,
+	}
 	addr := net.JoinHostPort(
 		os.Getenv("DD_AGENT_HOST"),
 		os.Getenv("DD_TRACE_AGENT_PORT"),
@@ -33,12 +45,20 @@ func main() {
 	// start the tracer with zero or more options
 	tracer.Start(tracer.WithServiceName("snowplow-schema-server"), tracer.WithAgentAddr(addr), tracer.WithEnv(ENV))
 	defer tracer.Stop()
-	mux := httptrace.NewServeMux() // init the http tracer
+	router := chi.NewRouter() // init the http tracer
+	router.Use(middleware.RequestID)
+	router.Use(middleware.RealIP)
+	router.Use(NewStructuredLogger(logger))
+	router.Use(middleware.Recoverer)
+	router.Use(chitrace.Middleware())
+	router.Use(middleware.Heartbeat("/ping"))
 
-	mux.Handle(PATH_BASE, http.FileServer(http.Dir("./schemas")))
+	router.Route(PATH_BASE, func(r chi.Router) {
+		r.Handle("/*", http.FileServer(http.Dir("./schemas")))
+	})
 
 	log.Println("Start listening on port:", PORT)
-	err := http.ListenAndServe(":"+PORT, mux) // set listen port
+	err := http.ListenAndServe(":"+PORT, router) // set listen port
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
